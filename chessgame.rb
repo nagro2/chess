48192cc073dd13719c7507b926c990f23015a903
nagro2 <nick_agro@hotmail.com>
2015-12-03 12:06:55 -0500

    Added a description of the Odin Bootcamp assignment

--- chessgame.rb
+++ chessgame.rb
@@ -1,7 +1,21 @@
 require_relative "chessgame_classes"
 
-# Classes for chessgame.rb are in chessgame_classes.rb
+=begin
+
+This is an interactive chess game creted as a Final Project in the Odin Bootcamp Ruby Programming section
+
+The assignment:
+Build a command line Chess game where two players can play against each other.
+The game should be properly constrained -- it should prevent players from making illegal moves and declare check or check mate in the correct situations.
+Make it so you can save the board at any time (remember how to serialize?)
+Write tests for the important parts. You don't need to TDD it (unless you want to), but be sure to use RSpec tests for anything that you find yourself typing into the command line repeatedly.
+
+
+Classes for chessgame.rb are in chessgame_classes.rb
+
+=end
 
 
 game = Game.new
 game.play
+
